4adf5b4fe3d54f97befe69644ce1ecccdf86f503
AshutoshRana1276 <aashutosh.ch.1276@gmail.com>
2022-09-29 12:54:27 +0530

    made calculator

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+// 	const myCollection = document.getElementsByTagName("button");
+// 	for (let i=0; i<myCollection.length; i++){
+// 		myCollection[i].addEventListener('click',()=>{
+
+// 		})
+// 		function mySecondFunction() {
+//   document.getElementById("demo").innerHTML += "Clicked!<br>";
+// }
+
+// 	}
+
