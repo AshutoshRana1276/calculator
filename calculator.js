d37c93aa3e5a1cfea1d8c519a9303c9ef923a299
AshutoshRana1276 <aashutosh.ch.1276@gmail.com>
2022-09-29 18:58:29 +0530

    7 attempt at fixing body height

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ let heightBtnRow = size.height;
 let height = window.innerHeight;
 
 document.querySelector(".screen").style.height = `${height - heightBtnRow}px`;
-document.querySelector(".formstyle").style.minHeight = `${height}px`;
+document.querySelector(".formstyle").style.height = `${height}px`;
 console.log(height);
 }
 
